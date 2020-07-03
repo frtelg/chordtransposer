23686b41c4b8d4c3850eadf669bd3028772c9598
Franke Telgenhof <franke.telgenhof@trivento.nl>
2020-07-03 11:51:15 +0200

    Initial commit

---
+++ src/main/kotlin/com/frtelg/chordtransposer/ChordtransposerApplication.kt
@@ -0,0 +1,11 @@
+package com.frtelg.chordtransposer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChordtransposerApplication
+
+fun main(args: Array<String>) {
+	runApplication<ChordtransposerApplication>(*args)
+}
---
+++ src/test/kotlin/com/frtelg/chordtransposer/ChordtransposerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.frtelg.chordtransposer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChordtransposerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
